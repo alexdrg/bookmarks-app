69528941bc8e64be9f45f41f0e4c84af6df81d37
Alexandru <alexandru@tempr.ai>
2024-12-08 15:21:21 +0200

    initial app setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'bookmarks', loadChildren: () => import('./components/bookmarks/bookmarks.module').then(m => m.BookmarksModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
